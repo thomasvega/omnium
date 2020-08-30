3ead12e0e174d822fafa89cb1bc059772e0114cd
TwikeySen <dontgonzalezvega@gmail.com>
2020-08-30 22:24:06 +0200

    Adding two more models

--- accounts/models.py
+++ accounts/models.py
@@ -26,3 +26,19 @@ class Member(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    media_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Wishlist(models.Model):
+    order = models.IntegerField(max_length=200, null=True)
+    item = models.ForeignKey(Item, null=True, on_delete=models.SET_NULL)
+    member = models.ForeignKey(Member, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.item.name
